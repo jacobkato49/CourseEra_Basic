54507981eafd9b7831f92cf0027776cbb23ad3e7
Jacob Kato <jacobkato49@yahoo.com>
2020-07-03 12:44:25 -0700

    tuples and lists

--- tutorial.py
+++ tutorial.py
@@ -5,7 +5,7 @@ z = 7
 
 a=200
 b=300
-
+#########
 
 print(p+w-z)
 print(a*b)
@@ -19,6 +19,8 @@ print('My name is', name, 'and my age is', age) #comma notation
 
 #type of variable using reserve keyword: type
 print(type(age))
+#########
+
 
 ##functions##
 def add_two_numbers(x,y):
@@ -27,3 +29,15 @@ def add_two_numbers(x,y):
 
 output = add_two_numbers(220,65)
 print(output)
+#########
+
+##lists##
+people = ['George', 'Megan', 'Allison']
+
+people.append('Watson')
+name = people.pop()
+print(name)
+print(people)
+
+people.remove('George')
+print(people)
