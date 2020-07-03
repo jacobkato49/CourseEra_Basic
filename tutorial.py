dee7106efd92951062b4e0b85043d4879835cf5b
Jacob Kato <jacobkato49@yahoo.com>
2020-07-03 12:13:47 -0700

    created a function

--- tutorial.py
+++ tutorial.py
@@ -1,15 +1,16 @@
-#simple mathematical expression
-x = 5
-y = 3
+##simple mathematical expression##
+p = 5
+w = 3
 z = 7
 
 a=200
 b=300
 
-print(x+y-z)
+
+print(p+w-z)
 print(a*b)
 
-#variable overview
+##variable overview##
 age =90
 name= 'John'
 
@@ -18,3 +19,11 @@ print('My name is', name, 'and my age is', age) #comma notation
 
 #type of variable using reserve keyword: type
 print(type(age))
+
+##functions##
+def add_two_numbers(x,y):
+    total= x + y
+    return total
+
+output = add_two_numbers(220,65)
+print(output)
