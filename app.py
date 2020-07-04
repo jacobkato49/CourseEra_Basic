9c3d60924a6f975b3654198a0e1af5eab0c28ba3
Jacob Kato <jacobkato49@yahoo.com>
2020-07-04 14:53:36 -0700

    added conditional statements to to-do list app to make it work more functionally

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 user_input= 'random'
+data=[]
 
 def show_menu():
     print('Menu:')
@@ -13,11 +14,22 @@ while user_input != '4':
     user_input = input('Enter your choice: ')
 
     if user_input == '1':
-        print('Add an item please')
+        item = input('What do you need to be done? ')
+        data.append(item)
+        print('Added item', item)
     elif user_input == '2':
-        print('Mark as done')
+        item = input('What is to be marked as done? ')
+
+        if item in data:
+            data.remove(item)
+            print('Removed item:', item)
+        else:
+            print('Item does not exist in the list')
+
     elif user_input == '3':
-        print('View the to-do items')
+        print('List of to-do items:')
+        for item in data:
+            print(item)
     elif user_input == '4':
         print('Goodbye')
     else:
